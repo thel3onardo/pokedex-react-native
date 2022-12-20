da5a3de116ba6551a6a493b49563b6a3d301a630
thel3onardo <leonardo.contato03@gmail.com>
2022-12-20 01:07:40 -0300

    feat: install expo-font package; setup project font family

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'SfProDisplay-bold': require('./assets/fonts/SFPRODISPLAYBOLD.otf'),
+    'SfProDisplay': require('./assets/fonts/SFPRODISPLAYREGULAR.otf'),
+    'SfProDisplay-medium': require('./assets/fonts/SFPRODISPLAYMEDIUM.otf'),
+    'SfProDisplay-semibold': require('./assets/fonts/SFPRODISPLAYSEMIBOLDITALIC.otf')
+  })
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'SfProDisplay-bold' }}>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
